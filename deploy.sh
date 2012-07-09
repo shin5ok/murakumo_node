8ae31bc8366af1ddd279ebb0609df64b8904fa5e
shin5ok <shin5ok@55mp.com>
2012-07-10 01:09:55 +0900

    deploy.sh modify

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo rsync -axv --delete -H --exclude ".svn" --exclude ".git" --exclude "deploy.sh" /home/kawano/murakumo_node/ /home/smc/murakumo_node/
+sudo rsync -axv --delete -H --exclude ".svn" --exclude ".git" --exclude "*.swp" --exclude "deploy.sh" $HOME/murakumo_node/ /home/smc/murakumo_node/
 sudo chown -R root.root /home/smc/murakumo_node/
 
