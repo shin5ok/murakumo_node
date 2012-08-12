4a64b59258e62cd922f60d58c54ae898a8aa17d2
shin5ok <shin5ok@55mp.com>
2012-08-12 13:06:21 +0900

    create setup/setup-init.pl for daemon init scripts

--- bin/daemon-init-set.sh
+++ bin/daemon-init-set.sh
@@ -1,38 +1,38 @@
 #!/bin/sh
 
-api_init="/etc/init.d/smc_vps2_node_api"
+api_init="/etc/init.d/murakumo_node_api"
 if ! test -e $api_init;
 then
   echo "$api_init make"
   cd /etc/init.d/
-  ln -s /home/smc/Murakumo_Node/bin/smc_vps2_node_api.init $api_init
+  ln -s /home/smc/murakumo_node/bin/murakumo_node_api.init $api_init
   chmod +x $api_init
   /sbin/chkconfig $api_init on
 fi
 
 
-job_init="/etc/init.d/smc_vps2_node_job"
+job_init="/etc/init.d/murakumo_node_job"
 if ! test -e $job_init;
 then
   echo "$job_init make"
   cd /etc/init.d/
-  ln -s /home/smc/Murakumo_Node/bin/job-worker.pl $job_init
+  ln -s /home/smc/murakumo_node/bin/job-worker.pl $job_init
   chmod +x $job_init
   /sbin/chkconfig $job_init on
 fi
 
-submit_init="/etc/init.d/smc_vps2_node_submit"
+submit_init="/etc/init.d/murakumo_node_submit"
 if ! test -e $submit_init;
 then
   echo "$submit_init make"
   cd /etc/init.d/
-  ln -s /home/smc/Murakumo_Node/bin/submit-my-condition.pl $submit_init
+  ln -s /home/smc/murakumo_node/bin/submit-my-condition.pl $submit_init
   chmod +x $submit_init
   /sbin/chkconfig $submit_init on
 fi
 
-$job_init stop
 echo "#########################################"
+$job_init stop
 $job_init start
 
 $submit_init stop
