8f0c06ccdc2e9ed8521abe5765e71dde65baab53
shin5ok <shin5ok@55mp.com>
2013-06-04 13:58:23 +0900

    modified to improve reliability when it select the remove files

--- bin/cleanup-unlinked.pl
+++ bin/cleanup-unlinked.pl
@@ -1,21 +1,25 @@
-#!/usr/bin/perl
+#!/usr/bin/env murakumo-perl
 use strict;
 use warnings;
 use File::Find;
-use Getopt::Long;
+use Carp;
+use opts;
 
-my $test;
-my $mday;
-GetOptions(
-            "test"   => \$test,
-            "mday=i" => \$mday,
-          );
-# デフォルト 7 day
-$mday ||= 7;
+use FindBin;
+use lib qq{$FindBin::Bin/../lib};
+use Murakumo_Node::CLI::Utils;
 
+opts my $test => { isa => 'Bool' },
+     my $mday => { isa => 'Int', default => 7 };
 my @dirs = @ARGV;
 
+if (@dirs == 0) {
+  croak "*** no dir args";
+}
+
 for my $dir ( @dirs ) {
+  $dir =~ m{^\s*/\s+}     and next;
+  $dir =~ m{^/[0-9a-z]+}i or  next;
   find(\&cleanup, $dir);
 }
 
@@ -23,9 +27,11 @@ sub cleanup {
   my $file = $File::Find::name;
   if (-f $file and $file =~ /\.unlinked$/) {
     if (-M $file > $mday) {
+      my $log = "try $file is removed";
       if ($test) {
-        print "try $file is removed\n"; 
+        print $log, "\n";
       } else {
+        logging $log;
         unlink $file;
       }
     }
@@ -34,3 +40,4 @@ sub cleanup {
   return;
 
 }
+
