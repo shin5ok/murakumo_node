64303b7c9a60762d3a58c5a814ca846d28fdf53c
shin5ok <shin5ok@55mp.com>
2013-07-01 18:20:34 +0900

    remove deploy.sh

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sudo rsync -axv --delete -H --exclude ".svn" --exclude ".git" --exclude "*.swp" --exclude "deploy.sh" --exclude "job.db" --delete $HOME/murakumo_node/ /home/smc/murakumo_node/
-sudo chown -R root.root /home/smc/murakumo_node/
-
